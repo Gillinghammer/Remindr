df7def31653d5635d6460cf5f4801d1799198c9c
Gillinghammer <colingillingham@gmail.com>
2014-11-23 21:55:14 -0600

    added wheneverize job

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'neat'
 gem 'angularjs-rails'
 gem "better_errors"
 gem "binding_of_caller"
+gem 'whenever', :require => false
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,8 @@ class UserMailer < ActionMailer::Base
 
   def remind_email(user)
     @user = User.find_by_id user
-    mail to: "#{@user.name} <#{@user.email}>", subject: "Your weekly reminder"
+    @contacts = @user.contacts.remind_this_week
+    mail(to: "#{@user.name} <#{@user.email}>", subject: "Your weekly reminder") unless @contacts.empty?
   end
 
 end
