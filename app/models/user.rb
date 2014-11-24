6cd5290af46d84ad1890bb9507dbbd4cae3f2c9a
Gillinghammer <colingillingham@gmail.com>
2014-11-23 20:25:26 -0600

    added recieve updates field and form field to users and registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :receive_updates
   # attr_accessible :title, :body
   has_many :contacts
   has_many :meetings
